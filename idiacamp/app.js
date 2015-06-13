1161775d6b5aed94620ee9115993a52a6bc58c0e
Peerapong Maitriwong <lactozily@Peerapongs-MacBook-Pro.local>
2015-06-14 06:00:23 +0700

    commit

--- idiacamp/app.js
+++ idiacamp/app.js
@@ -14,7 +14,7 @@ angular.module('idiacamp', [])
 				platform: 'Hardware',
 				picture: 'http://kawaii.kawaii.at/img/library-books-fabric-by-Timeless-Treasures-USA-171942-1.jpg',
 				team: '',
-				isSelected: true
+				isSelected: false
 			},
 			{
 				name: 'BTS/MRT Ticket',
